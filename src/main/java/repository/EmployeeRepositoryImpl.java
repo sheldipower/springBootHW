b12c05c43fdd196276d66979e684c2ed61e288a9
Irina <sheldipower@gmail.com>
2023-07-03 22:35:22 +0500

    1

--- src/main/java/repository/EmployeeRepositoryImpl.java
+++ src/main/java/repository/EmployeeRepositoryImpl.java
@@ -16,53 +16,7 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
             new Employee("Вика", 165_000)
     );
 
-    public int maxSalary() {
-        int max = 0;
-        for (int i = 0; i < EMPLOYEE_LIST.size(); i++)
-            if (EMPLOYEE_LIST.get(i).getSalary() > max) {
-                max = EMPLOYEE_LIST.get(i).getSalary();
-            }
-        return max;
-    }
 
-    @Override
-    public List<Employee> getAllEmployees() {
-        return new ArrayList<>(EMPLOYEE_LIST);
-    }
-
-    @Override
-    public int getAllEmployeeSumSalary() {
-        int sum = 0;
-        for (int i = 0; i < EMPLOYEE_LIST.size(); i++) {
-            sum += EMPLOYEE_LIST.get(i).getSalary();
-        }
-
-        return sum;
-    }
-
-    @Override
-    public String getEmployeeMinSalary() {
-        int min = maxSalary();
-        String minEmployee = null;
-        for (int i = 0; i < EMPLOYEE_LIST.size(); i++)
-            if (EMPLOYEE_LIST.get(i).getSalary() < min) {
-                min = EMPLOYEE_LIST.get(i).getSalary();
-                minEmployee = EMPLOYEE_LIST.get(i).getName();
-            }
-        return minEmployee;
-    }
-
-    @Override
-    public String getEmployeeMaxSalary() {
-        int max = 0;
-        String maxEmployee = null;
-        for (int i = 0; i < EMPLOYEE_LIST.size(); i++)
-            if (EMPLOYEE_LIST.get(i).getSalary() > max) {
-                max = EMPLOYEE_LIST.get(i).getSalary();
-                maxEmployee = EMPLOYEE_LIST.get(i).getName();
-            }
-        return maxEmployee;
-    }
 
     @Override
     public List<Employee> getEmployeeHighSalarySalary() {
--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -1,7 +1,9 @@
 package service;
 
+import pojo.Employee;
 import ru.skypro.lessons.springboot.weblibrary.pojo.Employee;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface EmployeeService {
@@ -10,4 +12,59 @@ public interface EmployeeService {
     String  getEmployeeMinSalary();
     String  getEmployeeMaxSalary();
     List<Employee>  getEmployeeHighSalarySalary();
+    private final static List<Employee> EMPLOYEE_LIST = List.of(
+            new Employee("Катя", 90_000),
+            new Employee("Дима", 102_000),
+            new Employee("Олег", 80_000),
+            new Employee("ОлЬга", 40_000),
+            new Employee("Вика", 165_000)
+    );
+
+    public int maxSalary() {
+        int max = 0;
+        for (int i = 0; i < EMPLOYEE_LIST.size(); i++)
+            if (EMPLOYEE_LIST.get(i).getSalary() > max) {
+                max = EMPLOYEE_LIST.get(i).getSalary();
+            }
+        return max;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return new ArrayList<>(EMPLOYEE_LIST);
+    }
+
+    @Override
+    public int getAllEmployeeSumSalary() {
+        int sum = 0;
+        for (int i = 0; i < EMPLOYEE_LIST.size(); i++) {
+            sum += EMPLOYEE_LIST.get(i).getSalary();
+        }
+
+        return sum;
+    }
+
+    @Override
+    public String getEmployeeMinSalary() {
+        int min = maxSalary();
+        String minEmployee = null;
+        for (int i = 0; i < EMPLOYEE_LIST.size(); i++)
+            if (EMPLOYEE_LIST.get(i).getSalary() < min) {
+                min = EMPLOYEE_LIST.get(i).getSalary();
+                minEmployee = EMPLOYEE_LIST.get(i).getName();
+            }
+        return minEmployee;
+    }
+
+    @Override
+    public String getEmployeeMaxSalary() {
+        int max = 0;
+        String maxEmployee = null;
+        for (int i = 0; i < EMPLOYEE_LIST.size(); i++)
+            if (EMPLOYEE_LIST.get(i).getSalary() > max) {
+                max = EMPLOYEE_LIST.get(i).getSalary();
+                maxEmployee = EMPLOYEE_LIST.get(i).getName();
+            }
+        return maxEmployee;
+    }
 }
